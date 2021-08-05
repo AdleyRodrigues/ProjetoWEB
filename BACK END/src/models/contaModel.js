536a9b7a2feb9024e22e5e5114e2d5ad59526187
Adley Rodrigues de Castro <51420674+AdleyRodrigues@users.noreply.github.com>
2021-08-05 09:56:41 -0300

    removendo versão antiga

--- BACK END/src/models/contaModel.js
+++
@@ -1,9 +0,0 @@
-class Conta {
-    constructor(nome, usuario, senha){
-        this.nome = nome;
-        this.usuario = usuario;
-        this.senha = senha;
-    }
-}
-
-module.exports = Conta;
--- BACK END/src/routes/loginRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const contaController = require("../controllers/contaController");
-
-router.post("/", contaController.verifica);
-
-module.exports = router;
-
