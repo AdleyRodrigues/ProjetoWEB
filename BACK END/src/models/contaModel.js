9d60e9e018900ef7e86bb69f92382bb9e9f91807
Adley Rodrigues de Castro <51420674+AdleyRodrigues@users.noreply.github.com>
2021-08-05 09:45:54 -0300

    commit back/front

---
+++ BACK END/src/models/contaModel.js
@@ -0,0 +1,9 @@
+class Conta {
+    constructor(nome, usuario, senha){
+        this.nome = nome;
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+}
+
+module.exports = Conta;
---
+++ BACK END/src/routes/contaRoute.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const contaController = require("../controllers/contaController");
+
+router.get("/", contaController.get);
+router.post("/", contaController.post);
+router.get("/:id", contaController.getById);
+router.put("/:id", contaController.put);
+router.delete("/:id", contaController.delete);
+
+module.exports = router;
+
